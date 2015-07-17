f96d89b856cb9483617e8bfc98796dd4cb998527
Nick Stafford <nicholas.stafford@webappuk.com>
2015-07-17 15:42:02 +0100

    Implement the TinyColor project I've included and also create a function to greyscale an entire page.

--- darkthemecode.js
+++ darkthemecode.js
@@ -1,3 +1,6 @@
+//TODOs:
+//	1. Write the script tag pulling in tinycolor in js.
+
 //Function to perform the DarkTheme operation
 var craigify = function() {
 	$('*').each(function() {
@@ -15,4 +18,38 @@ var craigify = function() {
 		$(this).css('border-weight', '20px');
 		$(this).css('color', 'white');
 	});
+};
+
+var greyscale = function () {
+	$('*').each(function() {
+		if($(this).css('background-color') != 'transparent' &&
+			$(this).css('background-color') != undefined) {
+			//Create a tinycolor object of the current element
+			var color = new tinycolor($(this).css('background-color'))
+			
+			//Greyscale color
+			color = color.greyscale();
+			
+			//push the greyscale color back to the element
+			$(this).css('background-color', color.toString());
+		}
+	});
+};
+
+//Function to take in a JQUERY object and greyscale it.
+var greyscaleElement = function(element)
+{
+			if($(element).css('background-color') != 'transparent' &&
+			$(this).css('background-color') != undefined) {
+			//Create a tinycolor object of the current element
+			var color = new tinycolor($(this).css('background-color'))
+			
+			//Greyscale color
+			color = color.greyscale();
+			
+			//push the greyscale color back to the element
+			$(this).css('background-color', color.toString());
+		}
+}
+
 };
